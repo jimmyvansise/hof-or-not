19b9d94643e9088459a1638bfe954ca87d3621bf
Jimmy Van Sise <jimmyvansise@hotmail.com>
2024-09-19 11:23:04 -0700

    fix(layout): clean up unnecessary fonts, and change metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import HomePage from "../pages/homepage";
 export default function Home() {
   return (
     <div className="min-h-screen p-8 pb-20 sm:p-20">
-            <HomePage></HomePage>
+      <HomePage></HomePage>
     </div>
   );
 }
