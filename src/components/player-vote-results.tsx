e71fa7e98418ba4b352e6ea53e5fa93b308834ca
Jimmy Van Sise <jimmyvansise@hotmail.com>
2024-11-23 17:16:56 -0800

    fix(images): Fix error with images related to widths

--- src/components/player-vote-results.tsx
+++ src/components/player-vote-results.tsx
@@ -2,7 +2,6 @@
 import React, { MouseEventHandler } from 'react';
 import clsx from 'clsx';
 import WideButton from '../components/wide-button';
-import Image from 'next/image';
 
 type PlayerVoteResultsProps = {
     playerName: string;
@@ -19,7 +18,7 @@ const renderPlayerImage = (picture: string, hofBorderColor: string): JSX.Element
   
     return (
         <div className='absolute top-0 left-0 h-full w-full flex justify-center'>
-            <Image
+            <img
                 className={clsx(`${hofBorderColor} border-4 h-full object-contain`)}
                 src={`data:image/jpeg;base64,${picture}`}
                 alt="Image"
