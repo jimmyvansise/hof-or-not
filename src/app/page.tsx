22d10f8ba4c288b004948535e3281d5b9de5d0de
Jimmy Van Sise <jimmyvansise@hotmail.com>
2024-11-26 07:40:31 -0800

    feat(maxwidth): set max width for main content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={`bg-hof-dark-blue min-h-screen pb-20`}>
       <TopBar />
-      <div className="px-8">
+      <div className="px-8 max-w-4xl mx-auto">
         <HomePage />
       </div>
     </div>
